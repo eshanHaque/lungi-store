069dbe6c34e419c5b93c6a8acd15c3d8c756d937
eshanHaque <eshan.haque16@gmail.com>
2022-04-04 23:14:56 +0600

    custom hook created

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import './Header.css';
 const Header = () => {
     return (
-        <div>
+        <div className='nav-container'>
             <nav>
                 <Link to="/home">Home</Link>
                 <Link to="/review">Review</Link>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,16 +5,20 @@ const Home = () => {
     return (
         <div>
             <div className='product-container'>
-            <div className='product-details'>
-                <div className='product-name'>
-                <h2>Lungi Galaxy Fold</h2>
-                <h2>You will be cold</h2>
+                <div className='product-details'>
+                    <div className='product-name'>
+                        <h2>Lungi Galaxy Fold</h2>
+                        <h2>You will be cold</h2>
+                    </div>
+                    <p>This is lungi for every human on earth. You can have many benifits for wearing a lungi. Its take one second for wearing a lungi. Lungi is very comfortable. You can use it anywhere in any place in the  world. You can aslo change cloth by using it. You can change cloth any where inthe world by using it. </p>
                 </div>
-                <p>This is lungi for every human on earth. You can have many benifits for wearing a lungi. Its take one second for wearing a lungi. Lungi is very comfortable. You can use it anywhere in any place in the  world. You can aslo change cloth by using it. You can change cloth any where inthe world by using it. </p>
+                <div className='product-image'>
+                    <img src={img} alt="" />
+                </div>
             </div>
-            <div className='product-image'>
-                <img src={img} alt="" />
-            </div>
+
+            <div className='reviews-container'>
+                <h2>Customer Review</h2>
             </div>
         </div>
     );
